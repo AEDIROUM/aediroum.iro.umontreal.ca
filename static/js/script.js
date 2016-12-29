6f72c9e5cfe1b53f00c962b7917c62b6d81216a5
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2016-12-29 14:54:42 -0500

    Better form

---
+++ static/js/script.js
@@ -0,0 +1,22 @@
+// Formulaire de contact
+$('#contact-form').submit(function (event) {
+    event.preventDefault();
+
+    $.ajax({
+        url: 'https://formspree.io/aediroum@iro.umontreal.ca',
+        method: 'POST',
+        data: $('#contact-form').serialize(),
+        dataType: 'json',
+        beforeSend: function () {
+            $('.alert-info').show();
+        },
+        success: function (data) {
+            $('.alert-info').hide();
+            $('.alert-success').show();
+        },
+        error: function (err) {
+            $('.alert-info').hide();
+            $('.alert-danger').show();
+        }
+    });
+});
