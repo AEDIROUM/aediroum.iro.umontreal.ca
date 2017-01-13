0c25967a1521b28e41ac622aa29e336c2dd8f345
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-01-13 15:25:09 -0500

    Some fixes to the contact form

--- themes/aediroum/static/js/main.js
+++ themes/aediroum/static/js/main.js
@@ -1,22 +1,28 @@
 // Formulaire de contact
-$('#contact').submit(function (event) {
-    event.preventDefault();
+var $form = $('#contact');
 
-    $.ajax({
-        url: 'https://formspree.io/aediroum@iro.umontreal.ca',
-        method: 'POST',
-        data: $('#contact-form').serialize(),
-        dataType: 'json',
-        beforeSend: function () {
-            // $('.alert-info').show();
-        },
-        success: function (data) {
-            // $('.alert-info').hide();
-            // $('.alert-success').show();
-        },
-        error: function (err) {
-            // $('.alert-info').hide();
-            // $('.alert-danger').show();
-        }
-    });
+$form.submit(function (event) {
+  event.preventDefault();
+
+  $.ajax({
+    url: 'https://formspree.io/aediroum@iro.umontreal.ca',
+    method: 'POST',
+    data: $form.serialize(),
+    dataType: 'json',
+    beforeSend: function () {
+      $('.alert-success').hide();
+      $('.alert-danger').hide();
+      $('.alert-info').show();
+    },
+    success: function () {
+      $('.alert-info').hide();
+      $('.alert-success').show();
+
+      $form[0].reset();
+    },
+    error: function () {
+      $('.alert-info').hide();
+      $('.alert-danger').show();
+    }
+  });
 });
