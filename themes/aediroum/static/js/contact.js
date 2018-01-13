b3131015dbb638fa767912cc32b9adc47e5ac509
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2018-01-13 12:26:21 -0500

    Update theme for Hugo 0.32

--- themes/aediroum/static/js/contact.js
+++ themes/aediroum/static/js/contact.js
@@ -1,27 +1,27 @@
 var $form = $('#contact');
 
 $form.submit(function (event) {
-  event.preventDefault();
+    event.preventDefault();
 
-  $.ajax({
-    url: 'https://formspree.io/aediroum@iro.umontreal.ca',
-    method: 'POST',
-    data: $form.serialize(),
-    dataType: 'json',
-    beforeSend: function () {
-      $('.alert-success').hide();
-      $('.alert-danger').hide();
-      $('.alert-info').show();
-    },
-    success: function () {
-      $('.alert-info').hide();
-      $('.alert-success').show();
+    $.ajax({
+        url: 'https://formspree.io/aediroum@iro.umontreal.ca',
+        method: 'POST',
+        data: $form.serialize(),
+        dataType: 'json',
+        beforeSend: function () {
+            $('.alert-success').hide();
+            $('.alert-danger').hide();
+            $('.alert-info').show();
+        },
+        success: function () {
+            $('.alert-info').hide();
+            $('.alert-success').show();
 
-      $form[0].reset();
-    },
-    error: function () {
-      $('.alert-info').hide();
-      $('.alert-danger').show();
-    }
-  });
+            $form.trigger("reset");
+        },
+        error: function () {
+            $('.alert-info').hide();
+            $('.alert-danger').show();
+        }
+    });
 });
